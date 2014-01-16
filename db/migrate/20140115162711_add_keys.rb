e079f787ac5b60564faaeaf5c695634f8ab73279
Bruce Steedman <bruce.steedman@gmail.com>
2014-01-16 09:54:28 +0000

    Added image_url attribute to Restaurant

--- db/migrate/20140115162711_add_keys.rb
+++
@@ -1,6 +0,0 @@
-class AddKeys < ActiveRecord::Migration
-  def change
-    add_foreign_key "reviews", "restaurants", name: "reviews_restaurant_id_fk"
-    add_foreign_key "reviews", "users", name: "reviews_user_id_fk"
-  end
-end
